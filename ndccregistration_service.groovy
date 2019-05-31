cf3c0cc6c5263848e32f7e2de660828e45221f7b
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-05-30 21:25:58 -0500

    Update ndccregistration_service.groovy

--- ndccregistration_service.groovy
+++ ndccregistration_service.groovy
@@ -2,8 +2,8 @@
    def mvnHome
    def gradleHome
 	
-  def stage('Preparation') { // for display purposes
-      // Get some code from a GitHub repository
+  def stage('Preparation') {
+      // or display purposes Get some code from a GitHub repository
       git'https://github.com/ndccroyals/ndcc-service-registration.git'
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
