51beb533da49953713f081fe371d8550ca6cbca9
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-05-30 21:51:07 -0500

    Update ndccregistration_service.groovy

--- ndccregistration_service.groovy
+++ ndccregistration_service.groovy
@@ -14,9 +14,9 @@ node{
 	   stage('Build') {
       // Run the maven build
       if (isUnix()) {
-         sh "'${mvnHome}/bin' mvn clean package"
+         sh "'${mvnHome}' clean package"
       } else {
-         bat(/"${mvnHome}\bin" mvn clean package/)
+         bat(/"${mvnHome}" clean package/)
       }
    }
    stage('gradle build') {
